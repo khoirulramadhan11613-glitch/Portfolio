188108ea49159ce01888e94dc8a6ab95c8e742db
khoirulramadhan11613-glitch <khoirulramadhan11613@gmail.com>
2026-06-20 03:51:31 +0700

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 90
+    },
+    color: {
+      value: "#777777"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.4
+    },
+    size: {
+      value: 3
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#999999",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  }
+});
